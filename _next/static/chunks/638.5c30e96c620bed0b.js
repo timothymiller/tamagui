e8a7131ea98a456b170a9363a204ff8d49d17183
natew <natew@users.noreply.github.com>
2022-05-26 10:03:05 +0000

    deploy: 8daceea6182d0a0830a7fc26e04df30ca06d4eed

---
+++ _next/static/chunks/638.5c30e96c620bed0b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[638],{36638:function(e,n,s){s.r(n),s.d(n,{default:function(){return u}});var c=s(52322),t=s(73456),r=s(57480);function u(){return(0,c.jsx)(t.b,{bc:"$pink10",animation:"bouncy",elevation:"$4",size:110,br:"$9",hoverStyle:{scale:1.2},pressStyle:{scale:.9},children:(0,c.jsx)(r.K7,{downscale:.75})})}}}]);
